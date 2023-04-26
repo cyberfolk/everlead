aaa130aab168739abde85c96253c98948910e0f0
cyberfolk <andrea1995ferrari.com>
2023-04-26 11:55:15 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import SiteHeader from "./components/PageHeader .vue";
+import SiteMain from "./components/PageMain.vue";
+import SiteFooter from "./components/PageFooter.vue";
+export default {
+  components: {
+    SiteHeader,
+    SiteMain,
+    SiteFooter,
+  },
+};
+</script>
+
+<template>
+  <PageHeader></PageHeader>
+  <PageMain></PageMain>
+  <PageFooter></PageFooter>
+</template>
+
+<style lang="scss" scoped></style>
