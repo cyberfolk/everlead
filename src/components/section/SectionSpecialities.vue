d5c73b696a22dcdb0192fd8103de37d014854a0f
cyberfolk <andrea1995ferrari.com>
2023-04-26 18:39:43 +0200

    Add col and img in Specialities

--- src/components/section/SectionSpecialities.vue
+++ src/components/section/SectionSpecialities.vue
@@ -7,7 +7,38 @@ export default {
 
 <template>
   <section>
-    <h2>Our specialities</h2>
+    <div class="w-75 m-auto">
+      <h2 class="text-center">Our specialities</h2>
+      <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, vitae.</p>
+      <div class="spacer m-auto"></div>
+      <div class="row row-cols-4">
+        <div class="col text-center">
+          <img src="../../assets/img/section/specialities/knowing.svg" alt="" />
+          <h5>Knowing</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+        </div>
+        <!-- /.col -->
+        <div class="col text-center">
+          <img src="../../assets/img/section/specialities/selling.svg" alt="" />
+          <h5>Selling</h5>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+        </div>
+        <!-- /.col -->
+        <div class="col text-center">
+          <img src="../../assets/img/section/specialities/learning.svg" alt="" />
+          <h5>Learning</h5>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
+        </div>
+        <!-- /.col -->
+        <div class="col text-center">
+          <img src="../../assets/img/section/specialities/contact.svg" alt="" />
+          <h5>Contact</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </div>
   </section>
 </template>
 <style lang="scss" scoped></style>
