57d9b32e87d3850451dfca822b25876964bb06c5
cyberfolk <andrea1995ferrari.com>
2023-04-26 17:07:44 +0200

    Add division in col

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -7,9 +7,35 @@ export default {
 
 <template>
   <footer>
-    <div class="container">PageFooter</div>
-    <!-- /.container -->
+    <section class="ms_footer_top bg_dark">
+      <div class="container">
+        <div class="row row-cols-4">
+          <div class="col"><h5>About</h5></div>
+          <!-- /.col -->
+          <div class="col"><h5>Twitter</h5></div>
+          <!-- /.col -->
+          <div class="col"><h5>Important Links</h5></div>
+          <!-- /.col -->
+          <div class="col"><h5>Contact me</h5></div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.container -->
+    </section>
+    <!-- /.ms_footer_top -->
+
+    <section class="ms_footer_bottom bg_primary">
+      <p class="text_light text-center">Copyright Everlead &#169;MikadoThemes</p>
+    </section>
+    <!-- /.ms_footer_bottom -->
   </footer>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "../assets/scss/partials/variables" as *;
+
+h5 {
+  color: $light;
+}
+</style>
--- src/components/section/SectionContact.vue
+++ src/components/section/SectionContact.vue
@@ -7,7 +7,15 @@ export default {
 
 <template>
   <section>
-    <div class="container">SectionContact</div>
+    <div class="container">
+      <div class="row row-cols-2">
+        <div class="col">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, sequi.</div>
+        <!-- /.col -->
+        <div class="col"><h2>Contact us</h2></div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </div>
     <!-- /.container -->
   </section>
 </template>
