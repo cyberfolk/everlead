842fbbccdad109feab423a23d5aff4af6435b67a
cyberfolk <andrea1995ferrari.com>
2023-04-28 01:17:06 +0200

    Fill Events with json and luxon

--- src/components/item/CardEvent.vue
+++ src/components/item/CardEvent.vue
@@ -1,33 +1,37 @@
 <script>
 export default {
   name: "CardEvent",
-  components: {},
   props: {
     title: String,
+    duration: String,
     date: String,
     location: String,
+  },
+  data() {
+    return { luxonDate: null };
+  },
+  created() {
+    this.luxonDate = luxon.DateTime.fromFormat(this.date, "MMM dd, yyyy");
   },
 };
 </script>
 
 <template>
   <div class="ms_card_event d-flex px-5 py-4 border-bottom">
-    <div class="ms_card_date_cell pe-3">
+    <div class="ms_card_cell pe-3">
       <div class="square p-2 text-center bg_primary text_lighter">
-        <div class="ms_card_date_day"><h6 class="mb-0">22</h6></div>
-        <div class="ms_card_date_month_year text_light fs_08">Jan, 2022</div>
+        <h6 class="mb-0">{{ luxonDate.toFormat("dd") }}</h6>
+        <div class="text_light fs_08">{{ luxonDate.toFormat("MMM, yyyy") }}</div>
       </div>
       <!-- /.square -->
     </div>
-    <!-- /.ms_card_date_cell -->
-    <div class="ms_card_info_cell">
-      <div class="ms_card_info_name">
-        <h5>{{ title }}</h5>
-      </div>
+    <!-- /.ms_card_cell -->
+    <div class="ms_card_cell">
+      <h5>{{ title }}</h5>
       <ul class="fa-ul ms-4">
         <li class="fs_08">
           <span class="fa-li"><i class="fa-regular fa-clock text_primary"></i></span>
-          <p class="mb-1">{{ date }}</p>
+          <p class="mb-1">{{ duration + ", " + date }}</p>
         </li>
         <li class="fs_08">
           <span class="fa-li"><i class="fa-solid fa-location-dot text_primary"></i></span>
@@ -36,7 +40,7 @@ export default {
       </ul>
       <div class="fw-bold fs_08">READ MORE</div>
     </div>
-    <!-- /.ms_card_info_cell -->
+    <!-- /.ms_card_cell -->
   </div>
   <!-- /.ms_card_event -->
 </template>
--- src/components/section/SectionEvents.vue
+++ src/components/section/SectionEvents.vue
@@ -1,9 +1,11 @@
 <script>
+import events from "../../data/section/events.json";
 import CardEvent from "../item/CardEvent.vue";
 export default {
   name: "SectionEvents",
-  components: {
-    CardEvent,
+  components: { CardEvent },
+  data() {
+    return { events: events };
   },
 };
 </script>
@@ -14,10 +16,7 @@ export default {
       <div class="ms_medium_container">
         <div class="ms_card bg_lighter">
           <div class="ms_card_title shadow-sm px-5 py-4"><h4>Upcoming Events</h4></div>
-          <!-- /.ms_card_title -->
-          <CardEvent :title="'Melbourn Coaching'" :date="'TODO LUXON'" :location="'Cambridge, MA, 02138, USA'" />
-          <CardEvent :title="'Milan Coaching'" :date="'TODO LUXON'" :location="'Cambridge, MA, 02138, USA'" />
-          <CardEvent :title="'Loa Angels Coaching'" :date="'TODO LUXON'" :location="'Cambridge, MA, 02138, USA'" />
+          <CardEvent v-for="e in events" :title="e.title" :duration="e.duration" :date="e.date" :location="e.location" />
         </div>
         <!-- /.ms_card -->
       </div>
@@ -35,7 +34,6 @@ section {
   background-image: url("../../assets/img/section/events/bg.jpg");
   background-repeat: no-repeat;
   background-size: cover;
-
   .ms_medium_container {
     padding: 8rem 0;
     .ms_card {
