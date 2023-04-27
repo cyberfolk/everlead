e4f1238fa65e1580e2901a69d9fff79f1fcb5814
cyberfolk <andrea1995ferrari.com>
2023-04-27 18:43:30 +0200

    Add StatsBar

---
+++ src/components/item/StatsBar.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  name: "StatsBar",
+  components: {},
+  props: {
+    name: String,
+    percentage: Number,
+  },
+};
+</script>
+
+<template>
+  <div class="ms_stat pb-4">
+    <div class="d-flex justify-content-between">
+      <h6>{{ name }}</h6>
+      <h6>{{ percentage }}%</h6>
+    </div>
+    <div class="bar">
+      <div :style="{ width: percentage + '%' }" class="bar up"></div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use "../../assets/scss/partials/variables" as *;
+.bar {
+  border-radius: 0.5rem;
+  height: 0.3rem;
+  position: relative;
+  background-color: $lightless;
+  z-index: 0;
+}
+.bar.up {
+  cursor: pointer;
+  border-radius: 0.5rem;
+  height: 0.3rem;
+  top: 0;
+  left: 0;
+  position: absolute;
+  background-color: $primary;
+  z-index: 1;
+}
+</style>
--- src/components/section/SectionStats.vue
+++ src/components/section/SectionStats.vue
@@ -1,7 +1,10 @@
 <script>
+import StatsBar from "../item/StatsBar.vue";
 export default {
   name: "SectionStats",
-  components: {},
+  components: {
+    StatsBar,
+  },
 };
 </script>
 
@@ -9,10 +12,20 @@ export default {
   <section id="ms_stats">
     <div class="ms_big_container">
       <div class="ms_medium_container">
-        <div class="row row-cols-2">
-          <div class="col"><h2>Creative Leader.</h2></div>
+        <div class="row row-cols-2 g-5">
+          <div class="col">
+            <h2>Creative Leader.</h2>
+            <div class="spacer pb-3"></div>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium eveniet quae pariatur? Quisquam, a soluta! Excepturi, error velit. Impedit, quia accusantium? Aliquid, fuga amet? Aut alias repellat perspiciatis neque quaerat vitae velit, adipisci corrupti atque!</p>
+            <span class="fs_08 fw-bold text_dark">READ MORE</span>
+          </div>
           <!-- /.col -->
-          <div class="col">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, dolorem?</div>
+          <div class="col">
+            <StatsBar :name="'Mentorship'" :percentage="78" />
+            <StatsBar :name="'Education'" :percentage="95" />
+            <StatsBar :name="'Learning'" :percentage="65" />
+            <StatsBar :name="'Motivation'" :percentage="83" />
+          </div>
           <!-- /.col -->
         </div>
         <!-- /.row -->
@@ -23,4 +36,9 @@ export default {
   </section>
   <!-- /#ms_stats -->
 </template>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "../../assets/scss/partials/variables" as *;
+.ms_big_container {
+  padding: 8rem 0;
+}
+</style>
