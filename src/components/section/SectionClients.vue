e7df480ced9af37c06274db46125020a8d31ad8c
cyberfolk <andrea1995ferrari.com>
2023-04-27 22:12:01 +0200

    Add img

--- src/components/section/SectionClients.vue
+++ src/components/section/SectionClients.vue
@@ -6,9 +6,17 @@ export default {
 </script>
 
 <template>
-  <section id="ms_team">
+  <section id="ms_team" class="bg_light">
     <div class="ms_big_container">
-      <div class="ms_medium_container">SectionClients</div>
+      <div class="ms_medium_container">
+        <div class="d-flex justify-content-between">
+          <img src="../../assets/img/section/clients/01.png" alt="" />
+          <img src="../../assets/img/section/clients/02.png" alt="" />
+          <img src="../../assets/img/section/clients/03.png" alt="" />
+          <img src="../../assets/img/section/clients/04.png" alt="" />
+          <img src="../../assets/img/section/clients/05.png" alt="" />
+        </div>
+      </div>
       <!-- /.ms_medium_container -->
     </div>
     <!-- /.ms_big_container -->
