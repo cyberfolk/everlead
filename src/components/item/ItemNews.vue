821da19851232a6c4acf77dbaf0b4130530da427
cyberfolk <andrea1995ferrari.com>
2023-04-27 19:35:58 +0200

    Add ItemNews

---
+++ src/components/item/ItemNews.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  name: "SectionBlog",
+  components: {},
+  props: {
+    path: String,
+    date: String,
+    author: String,
+    title: String,
+    text: String,
+  },
+  methods: {
+    srcPath(path) {
+      return `src/assets/img/section/blog/${path}`;
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="ms_news">
+    <img class="w-100 pb-4" :src="srcPath(path)" alt="" />
+    <div class="fs_08 pb-2">
+      <i class="text_primary fa-regular fa-clock pe-1"></i>
+      <span class="pe-2">{{ date }}</span>
+      <i class="text_primary fa-solid fa-user pe-1"></i>
+      <span>{{ author }}</span>
+    </div>
+    <h5>{{ title }}</h5>
+    <p>{{ text }}</p>
+    <div class="fs_08 fw-bold">READ MORE</div>
+  </div>
+  <!-- /.ms_news -->
+</template>
+<style lang="scss" scoped>
+@use "../../assets/scss/partials/variables" as *;
+</style>
--- src/components/section/SectionBlog.vue
+++ src/components/section/SectionBlog.vue
@@ -1,9 +1,11 @@
 <script>
 import ItemSlider from "../item/ItemSlider.vue";
+import ItemNews from "../item/ItemNews.vue";
 export default {
   name: "SectionBlog",
   components: {
     ItemSlider,
+    ItemNews,
   },
 };
 </script>
@@ -12,7 +14,28 @@ export default {
   <section id="ms_blog">
     <div class="ms_big_container">
       <div class="ms_medium_container">
-        <h2>Latest news</h2>
+        <div class="text-center">
+          <h2>Latest news</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+          <div class="spacer m-auto pb-5"></div>
+        </div>
+        <div class="row row-cols-3 g-5">
+          <div class="col">
+            <ItemNews :path="'01.jpg'" :date="'May 6 2019'" :author="'Amanda Doe'" :title="'Next Investiment'" :text="'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, veniam!'" />
+          </div>
+          <!-- /.col -->
+          <div class="col">
+            <ItemNews :path="'01.jpg'" :date="'May 6 2019'" :author="'Amanda Doe'" :title="'Next Investiment'" :text="'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, veniam!'" />
+          </div>
+          <!-- /.col -->
+          <div class="col">
+            <ItemNews :path="'01.jpg'" :date="'May 6 2019'" :author="'Amanda Doe'" :title="'Next Investiment'" :text="'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, veniam!'" />
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+
+        <!-- /.spacer -->
         <ItemSlider />
       </div>
       <!-- /.ms_medium_container -->
@@ -21,4 +44,10 @@ export default {
   </section>
   <!-- /#ms_blog -->
 </template>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "../../assets/scss/partials/variables" as *;
+#ms_blog {
+  background-color: $light;
+  padding: 8rem 0;
+}
+</style>
