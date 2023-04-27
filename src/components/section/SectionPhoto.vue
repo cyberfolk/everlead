fdd9034992659a6b2d6a22dbaffc60db76461779
cyberfolk <andrea1995ferrari.com>
2023-04-27 21:54:04 +0200

    Add img

--- src/components/section/SectionPhoto.vue
+++ src/components/section/SectionPhoto.vue
@@ -6,6 +6,19 @@ export default {
 </script>
 
 <template>
-  <section>SectionPhoto</section>
+  <section id="ms_photo">
+    <img class="w-100" src="../../assets/img/section/photo/03.jpg" alt="" />
+    <i class="play fa-solid fa-play text_light fa-5x"></i>
+  </section>
+  <!-- /#ms_photo -->
 </template>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#ms_photo {
+  position: relative;
+  .play {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+  }
+}
+</style>
