63c9983c60c5fd5378411a98fd4df3507dfd8221
cyberfolk <andrea1995ferrari.com>
2023-04-28 16:10:33 +0200

    Make scrollable the testimonials with buttons

--- src/components/section/SectionTestimonials.vue
+++ src/components/section/SectionTestimonials.vue
@@ -6,29 +6,52 @@ export default {
   components: { ItemSlider },
   data() {
     return {
+      activeImage: 0,
       percentage: 33,
       testimonials: testimonials,
     };
+  },
+  methods: {
+    next() {
+      this.activeImage++;
+      if (this.activeImage == this.testimonials.length) {
+        this.activeImage = 0;
+      }
+      this.calcPercentage();
+    },
+    prev() {
+      this.activeImage--;
+      if (this.activeImage < 0) {
+        this.activeImage = this.testimonials.length - 1;
+      }
+      this.calcPercentage();
+    },
+    calcPercentage() {
+      this.percentage = Math.ceil((100 * (this.activeImage + 1)) / this.testimonials.length);
+    },
   },
 };
 </script>
 
 <template>
   <section id="ms_testimonials" class="bg_darkless">
-    <div class="bg_text text_dark"></div>
+    <div class="bg_text text_dark position-absolute top-0 start-50 translate-middle-x">Testimonials.</div>
     <!-- /.bg_text -->
     <div class="ms_big_container">
+      <button class="ms_arrow bg-transparent text_light border-0 start-0" @click="prev()">&LongLeftArrow;</button>
+      <button class="ms_arrow bg-transparent text_light border-0 end-0" @click="next()">&LongRightArrow;</button>
       <div class="ms_medium_container">
         <div class="m-auto text-center">
-          <img class="border border-3 rounded-circle border-white m-3" :src="testimonials[0].path" alt="" />
-          <h5 class="text_light m-4">{{ testimonials[0].name }}</h5>
-          <p class="w-50 m-auto text-center pb-4">{{ testimonials[0].text }}</p>
+          <img class="border border-3 rounded-circle border-white m-3" :src="testimonials[activeImage].path" alt="" />
+          <h5 class="text_light m-4">{{ testimonials[activeImage].name }}</h5>
+          <p class="w-50 m-auto text-center pb-4">{{ testimonials[activeImage].text }}</p>
+
           <div class="d-flex align-items-center justify-content-center">
-            <div class="text_light">01</div>
-            <div class="bar">
-              <div :style="{ width: percentage + '%' }" class="bar up"></div>
+            <div class="text_light">{{ "0" + (activeImage + 1) }}</div>
+            <div class="bar m-2">
+              <div :style="{ width: percentage + '%' }" class="bar up m-0"></div>
             </div>
-            <div class="text_light">03</div>
+            <div class="text_light">{{ "0" + testimonials.length }}</div>
           </div>
         </div>
       </div>
@@ -44,17 +67,9 @@ export default {
 
 section {
   position: relative;
-  .bg_text::after {
-    content: "Testimonials.";
-    text-align: center;
-    font-size: 200px !important;
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    overflow: hidden;
-    line-height: 0.9;
+  .bg_text {
+    font-size: 200px;
+    line-height: 1;
     font-family: "Libre Baskerville", serif;
   }
   .ms_big_container {
@@ -63,23 +78,12 @@ section {
     z-index: 1;
   }
   .bar {
-    border-radius: 0.5rem;
     height: 0.1rem;
-    position: relative;
     background-color: $secondary;
-    z-index: 0;
-    margin: 0 0.5rem;
     width: 10%;
   }
   .bar.up {
-    cursor: pointer;
-    top: 0;
-    left: 0;
-    position: absolute;
     background-color: $light;
-    margin: 0rem;
-    z-index: 1;
-    width: 33%;
   }
 }
 </style>
